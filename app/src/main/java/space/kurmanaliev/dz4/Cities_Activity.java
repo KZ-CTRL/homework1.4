5d2f86bb0aa971fe2ee7021a1fc4152ea7a7b145
KZ-CTRL <kairat020998gmail.com>
2020-12-16 17:45:03 +0300

    Initial commit

---
+++ app/src/main/java/space/kurmanaliev/dz4/Cities_Activity.java
@@ -0,0 +1,6 @@
+package space.kurmanaliev.dz4;
+
+import android.app.Activity;
+
+public class Cities_Activity extends Activity {
+}
---
+++ app/src/main/java/space/kurmanaliev/dz4/MainActivity.java
@@ -0,0 +1,14 @@
+package space.kurmanaliev.dz4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
